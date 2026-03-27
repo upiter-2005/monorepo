94f7f894ec3dfcb2320cea4baaba4c06c33e9866
upiter-2005 <pavel470245@gmail.com>
2026-03-27 10:06:38 +0200

    fixed tokens util naming

--- apps/web/src/store/auth/AuthProvider.tsx
+++ apps/web/src/store/auth/AuthProvider.tsx
@@ -6,7 +6,7 @@ import { jwtDecode } from 'jwt-decode';
 import { useNavigate } from 'react-router-dom';
 
 import { AuthContext } from './AuthContext';
-import { getHash, setHash, removeHash } from '../../share/helpers/tokenHash';
+import { getToken, setToken, removeToken } from '../../share/helpers/token';
 import { ROUTES } from '../../share/routes';
 import type { CredentialResponseData } from '../../types/authTypes';
 interface AuthProviderProps {
@@ -19,7 +19,7 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
 
   const cleanToken = () => {
     googleLogout();
-    removeHash();
+    removeToken();
     navigate(ROUTES.LOGIN);
   };
 
@@ -27,14 +27,14 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
     const crd = credentialResponseData.credential;
     if (crd) {
       const decoded: CredentialResponse = jwtDecode(crd);
-      setHash(crd);
+      setToken(crd);
       if (decoded) navigate(ROUTES.HOME);
     }
   };
 
   useEffect(() => {
     const setupToken = () => {
-      const credentialHash = getHash();
+      const credentialHash = getToken();
       if (credentialHash) {
         const decodedToken: CredentialResponseData =
           jwtDecode<CredentialResponseData>(credentialHash);
