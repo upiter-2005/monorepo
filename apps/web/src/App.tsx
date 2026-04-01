4a1946a6456cd90d07438debc76d5f616010ec3f
Yevhen Poliakov <yevhenpoliakov@MacBook-Pro-Yevhen.local>
2026-04-01 17:23:44 +0300

    add users route

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navigation';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Users from './pages/Users';
 import { ROUTES } from './share/routes';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path={ROUTES.HOME} element={<Home />} />
         <Route path={ROUTES.LOGIN} element={<Login />} />
+        <Route path={ROUTES.USERS} element={<Users />} />
       </Routes>
     </div>
   );
